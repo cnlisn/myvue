9b367c9df5888a5a5964b1747f5e6b1eecc25588
cnlisn <cnlishan@163.com>
2019-12-22 09:58:57 +0800

    添加登录和主页面

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+//引入自己的通用css
+import '../static/css/common.css'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+
+
+
 new Vue({
   el: '#app',
   router,
